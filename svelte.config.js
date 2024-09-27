69b6c679a85f5e737494c46a6bfb2d73463688d4
Fancisco Martinez <francisco.martinez@bluewebsoft.com>
2024-09-27 14:50:09 -0600

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({status, path, referrer, referenceType }) =>{
+				if(status === 404){
+					console.warn(`404 error on prerendering: ${path}`);
+					return;
+				}
+				throw new Error(`${status} ${path} (linked from ${referrer})`);
+			}
 	},
 	preprocess: vitePreprocess()
 };
