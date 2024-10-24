86eeb769f5bc07a0397b8b6af3a75562a9baddb8
Fancisco Martinez <francisco.martinez@bluewebsoft.com>
2024-10-24 17:22:06 -0600

    Validar Qr prueba haciendo update en tabla tickets

--- src/routes/validate/succesValidate/+page.svelte
+++ src/routes/validate/succesValidate/+page.svelte
@@ -22,6 +22,7 @@
 			toast.success('Referencia Validada con exito', {
 				duration: 4000
 			});
+            return;
 		}
 	});
 </script>
@@ -30,4 +31,21 @@
 
 <h1>Referencia valida: {referencia}</h1>
 
-<a href="/validate">Seguir validando</a>
+<button class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+    <a href="/validate">Seguir validando</a>
+</button>
+
+<style>
+	button {
+		background-color: rgb(63, 248, 186);
+		color: black;
+	}
+
+	button:hover {
+		background-color: rgb(52, 180, 137);
+	}
+
+    a{
+        color: black;
+    }
+</style>
